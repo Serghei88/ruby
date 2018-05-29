ba8f8663cfff57479b06c7f6122a2a52521c9981
Serghei <it.serghei@gmail.com>
2018-05-29 11:44:03 +0300

    Pig Latin (1p)

---
+++ task5_pig_latin.rb
@@ -0,0 +1,12 @@
+
+def pig_latin(str)
+  words = str.split(' ')
+  result = ''
+  words.each do |word|
+    result += word[1..-1] + word[0, 1].downcase + 'ay '
+  end
+  result[0] = result[0].upcase
+  puts result
+end
+
+pig_latin('The quick brown fox')
