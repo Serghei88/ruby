5af9ded7a1e3cb4da63c92867df3bdea70062880
Serghei <it.serghei@gmail.com>
2018-05-30 11:00:47 +0300

    code review fix

--- task1_codeFormating.rb
+++ task1_codeFormating.rb
@@ -1,37 +1,37 @@
 def format_duration(seconds)
   hours = seconds / 3600
   minutes = (seconds - (hours * 3600)) / 60
-  remainingSeconds = seconds - (hours * 3600) - (minutes * 60)
+  remaining_seconds = seconds - (hours * 3600) - (minutes * 60)
 
-  hoursPart = ''
-  hoursPart = case hours
-              when 0
-                ''
-              when 1
-                hours.to_s + ' hour'
-              else
-                hours.to_s + ' hours'
+  hours_part = ''
+  hours_part = case hours
+               when 0
+                 ''
+               when 1
+                 hours.to_s + ' hour'
+               else
+                 hours.to_s + ' hours'
               end
 
-  minutesPart = ''
-  minutesPart = case minutes
-                when 0
-                  ''
-                when 1
-                  minutes.to_s + ' minute'
-                else
-                  minutes.to_s + ' minutes'
+  minutes_part = ''
+  minutes_part = case minutes
+                 when 0
+                   ''
+                 when 1
+                   minutes.to_s + ' minute'
+                 else
+                   minutes.to_s + ' minutes'
                 end
 
-  secondsPart = ''
-  secondsPart = case remainingSeconds
-                when 0
-                  ''
-                when 1
-                  remainingSeconds.to_s + ' second'
-                else
-                  remainingSeconds.to_s + ' seconds'
+  seconds_part = ''
+  seconds_part = case remainingSeconds
+                 when 0
+                   ''
+                 when 1
+                   remainingSeconds.to_s + ' second'
+                 else
+                   remainingSeconds.to_s + ' seconds'
                 end
 
-  hoursPart + (hours > 0 && minutes > 0 ? ', ' : '') + minutesPart + (minutes > 0 && remainingSeconds > 0 ? ' and ' : '') + secondsPart
+  hoursPart + (hours > 0 && minutes > 0 ? ', ' : '') + minutes_part + (minutes > 0 && remaining_seconds > 0 ? ' and ' : '') + seconds_part
 end
