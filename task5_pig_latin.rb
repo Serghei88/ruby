a67da2652cef4e20a721d7ea26d0767dcf25b8e5
Serghei <it.serghei@gmail.com>
2018-05-30 14:46:45 +0300

    review changes

--- task5_pig_latin.rb
+++ task5_pig_latin.rb
@@ -1,10 +1,7 @@
 
 def pig_latin(str)
   words = str.split(' ')
-  result = ''
-  words.each do |word|
-    result += word[1..-1] + word[0, 1].downcase + 'ay '
-  end
+  result = words.reduce('') { |res, word| res + word[1..-1] + word[0, 1].downcase + 'ay ' }
   result[0] = result[0].upcase
   puts result
 end
