3dffdc857dac5a2ac3dae6f6e42374578680ee56
Serghei <it.serghei@gmail.com>
2018-05-30 14:06:26 +0300

    fix according review

--- task2_multiples3_5.rb
+++ task2_multiples3_5.rb
@@ -1,7 +1,5 @@
 def solution(number)
-  result = 0
-  (0..number - 1).each do |i|
-    result += i % 3 == 0 || i % 5 == 0 ? i : 0
-  end
-  result
+  (number - 1).downto(3).reduce(0) { |result, i| result + (i % 3 == 0 || i % 5 == 0 ? i : 0) }
 end
+
+puts solution(10)
