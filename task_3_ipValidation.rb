1681ffef834cbedb27658037aa5fdd8c2058c0ad
Serghei <it.serghei@gmail.com>
2018-05-30 14:16:06 +0300

    core review change

--- task_3_ipValidation.rb
+++ task_3_ipValidation.rb
@@ -1,20 +1,11 @@
-def isValidIP(str)
+def valid_ip(str)
+  numbers = str.split('.')
 
-    numbers = str.split('.')
-
-    if (numbers.size <=> 4) != 0
-      return false
-    end
-  
-    numbers.each do |s|
-      if s.to_i < 0 || s.to_i > 255 || (s.start_with?("0") && s.size>1) 
-        return false
-      end
-    end
-    return true;
-  end
-  puts isValidIP("1.2.3.4")
-  puts isValidIP("123.45.67.89")
-  puts isValidIP("1.2.3")
-  puts isValidIP("1.2.3.4.5")
-  puts isValidIP("123.045.067.089")
+  return false unless numbers.size == 4
+  numbers.all? { |s| (s.to_i > 0 && s.to_i < 255 && (!s.start_with?('0') && !s.empty?)) }
+end
+puts valid_ip('1.2.3.4')
+puts valid_ip('123.45.67.89')
+puts valid_ip('1.2.3')
+puts valid_ip('1.2.3.4.5')
+puts valid_ip('123.045.067.089')
