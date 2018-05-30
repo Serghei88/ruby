1625d94c11c3235ec4fa13c03c2d69eb813904df
Serghei <it.serghei@gmail.com>
2018-05-30 15:05:44 +0300

    review changes

--- task_sudoku.rb
+++ task_sudoku.rb
@@ -1,54 +1,63 @@
 
-def checkSudokuLine(line)
+def sudoku_line_valid?(line)
+  line.sort == [1, 2, 3, 4, 5, 6, 7, 8, 9]
+end
+
+def sudokku_lines_valid?(sudokku_array)
   result = true
-  (1..9).each do |i|
-    result = false unless line.include? i
+  # check lines
+  sudokku_array.each do |line|
+    result = false unless sudoku_line_valid?(line)
   end
+  puts('check lines: ' + result.to_s)
   result
 end
 
-def checkQuadrant(i, j, list)
-  tempList = []
-  (i..i + 2).each do |r|
-    (j..j + 2).each do |k|
-      tempList.push(list[r][k])
-    end
-  end
-  checkSudokuLine(tempList)
-end
-
-def sudoku(listcolumns)
+def sudokku_columns_valid?(sudokku_array)
   result = true
-  # check lines
-  listcolumns.each do |line|
-    result = false unless checkSudokuLine(line)
-  end
-  puts('check lines: ' + result.to_s)
   # check columns
-  (0..listcolumns.size - 1).each do |rowNumber|
+  (0..sudokku_array.size - 1).each do |row_number|
     # if rows numbers not equal to column number - this is not sudokku
-    return false unless listcolumns[rowNumber].size == listcolumns.size
+    return false unless sudokku_array[row_number].size == sudokku_array.size
 
-    tempColumn = []
-    (0..listcolumns[rowNumber].size - 1).each do |columnNumber|
-      tempColumn.push(listcolumns[columnNumber][rowNumber])
+    temp_column = []
+    (0..sudokku_array[row_number].size - 1).each do |column_number|
+      temp_column.push(sudokku_array[column_number][row_number])
     end
-    result = false unless checkSudokuLine(tempColumn)
+    result = false unless sudoku_line_valid?(temp_column)
   end
   puts('check columns: ' + result.to_s)
-  # check quadrants, num of columns already equal with num of rows
-  return false unless listcolumns.size % 3 == 0
-  numOfQuadrantsInLine = listcolumns.size / 3
-  i = 0
-  while i < listcolumns.size
-    j = 0
-    while j < listcolumns.size
-      puts ' checking quadrant i=' + i.to_s + ' to ' + (i + 3).to_s +
-           '  j=' + j.to_s + ' to' + (j + 3).to_s + ' result = ' +
-           checkQuadrant(i, j, listcolumns).to_s
-      j += 3
+  result
+end
+
+def quadrant_valid?(i, j, list)
+  temp_list = []
+  (i..i + 2).each do |r|
+    (j..j + 2).each do |k|
+      temp_list.push(list[r][k])
     end
-    i += 3
+  end
+  puts ' checking quadrant i=' + i.to_s + ' to ' + (i + 3).to_s +
+       '  j=' + j.to_s + ' to' + (j + 3).to_s + ' result = ' +
+       sudoku_line_valid?(temp_list).to_s
+
+  sudoku_line_valid?(temp_list)
+end
+
+def sudoku(sudokku_array)
+  quadrant_size = 3
+  return false unless sudokku_array.size % quadrant_size == 0
+  result = sudokku_lines_valid?(sudokku_array) && sudokku_columns_valid?(sudokku_array)
+
+  
+  i = 0
+  while i < sudokku_array.size
+    j = 0
+    while j < sudokku_array.size
+      result &&= quadrant_valid?(i, j, sudokku_array)
+      j += quadrant_size
+    end
+    i += quadrant_size
   end
 
   result
@@ -73,4 +82,6 @@ validSudoku = [[4, 3, 5, 2, 6, 9, 7, 8, 1],
                [2, 4, 8, 9, 5, 7, 1, 3, 6],
                [7, 6, 3, 4, 1, 8, 2, 5, 9]]
 puts sudoku(validSudoku)
-# puts checkQuadrant(0,0,testArray)
+puts sudoku(testArray)
+
+#puts quadrant_valid?(0,0,testArray)
